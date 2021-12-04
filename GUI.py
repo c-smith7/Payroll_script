c1d79cb7a81576fa1234fbf61841e966006dfeee
Carlos Smith <mcmco421@gmail.com>
2021-12-04 10:21:52 -0500

    Update and edit qy slots, signals, and threads for GUI.

--- GUI.py
+++ GUI.py
@@ -16,7 +16,6 @@ class MainWindow(QDialog):
         self.app_icon.addFile('quikmed-logo.ico', QtCore.QSize(16, 16))
         self.setWindowIcon(self.app_icon)
         self.threadpool = QThreadPool()
-        self.payroll_complete.setVisible(False)
         self.prog_bar.setVisible(False)
         self.open_file_btn.setVisible(False)
         self.open_file_btn.setEnabled(False)
@@ -32,12 +31,6 @@ class MainWindow(QDialog):
     def generate_payroll(self):
         file = self.file_name.text()
         format_script(file)
-        
-    def payroll_complete_msg(self):
-        self.payroll_complete.setVisible(True)
-
-    def payroll_complete_msg_close(self):
-        self.payroll_complete.setVisible(False)
 
     def progress_bar(self):
         self.prog_bar.setVisible(True)
@@ -62,15 +55,11 @@ class MainWindow(QDialog):
     def generate_slots(self):
             worker = WorkerThread(self.generate_payroll)
             worker.signal.prog_start.connect(self.progress_bar)
-            worker.signal.complete.connect(self.payroll_complete_msg)
-            worker.signal.reset.connect(self.payroll_complete_msg_close)
             worker.signal.open_file_btn.connect(self.open_file_btn_enabled)
             self.threadpool.start(worker)
 
 
 class WorkerSignals(QObject):
-    complete = pyqtSignal()
-    reset = pyqtSignal()
     prog_start = pyqtSignal()
     open_file_btn = pyqtSignal()
 
@@ -87,12 +76,9 @@ class WorkerThread(QRunnable):
     @pyqtSlot()
     def run(self):
             self.fn(*self.args, **self.kwargs)
-            time.sleep(0.05)
+            time.sleep(0.03)
             self.signal.prog_start.emit()
-            time.sleep(1)
-            self.signal.complete.emit()
-            time.sleep(3)
-            self.signal.reset.emit()
+            time.sleep(0.5)
             self.signal.open_file_btn.emit()
 
             
